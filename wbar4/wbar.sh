52445ca3a884b869678702c54f96d74784caf7f5
frank038 <42263206+frank038@users.noreply.github.com>
2025-05-04 08:57:44 +0200

    Add files via upload

--- wbar4/wbar.sh
+++ wbar4/wbar.sh
@@ -2,5 +2,5 @@
 
 thisdir=$(dirname "$0")
 cd $thisdir
-python3 wbar.py &
-# LD_PRELOAD=./libgtk4-layer-shell.so.1.0.4 python3 wbar.py
+# python3 wbar.py &
+LD_PRELOAD=./libgtk4-layer-shell.so.1.0.4 python3 wbar.py
