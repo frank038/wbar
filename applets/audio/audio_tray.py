55a36374bf3f383651afb7d09d7c8ec595c86f18
frank038 <42263206+frank038@users.noreply.github.com>
2025-12-29 11:02:43 +0100

    Add files via upload

--- applets/audio/audio_tray.py
+++ applets/audio/audio_tray.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# V. 0.5
+# V. 0.5.1
 
 from PyQt6.QtWidgets import QSystemTrayIcon, QMenu, QLabel, QWidget, QApplication, QBoxLayout, QPushButton, QSlider, QRadioButton, QCheckBox
 from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QHBoxLayout, QListWidget, QListWidgetItem, QSizePolicy, QScrollArea
@@ -22,6 +22,7 @@ _TRAY = None
 class myMenu(QMenu):
     def __init__(self):
         super(myMenu, self).__init__()
+        self.setWindowTitle("qtaudio1")
         self.pixel_ratio = self.devicePixelRatio()
         # 1
         self.scroll = QScrollArea()
