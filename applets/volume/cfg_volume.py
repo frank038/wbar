1764baf9fc8cc0463e3cc395e26b9907c2dfd3d8
frank038 <42263206+frank038@users.noreply.github.com>
2026-01-19 16:04:34 +0100

    Add files via upload

--- applets/volume/cfg_volume.py
+++ applets/volume/cfg_volume.py
@@ -3,8 +3,6 @@ WIN_WIDTH = 600
 WIN_HEIGHT = 300
 # audio icon button size
 button_size = 64
-# increase if the window opens again after it has been closed
-reset_timer = 150
 # volume step: %
 AUDIO_STEP = 4
 # audio control panel command
